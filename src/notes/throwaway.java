0c28d9a689a0fd91d3d482a4c26c5a6ffe5fb8cd
justin951 <justin951@revature.com>
2024-09-11 11:57:09 -0400

    update

--- src/notes/throwaway.java
+++ src/notes/throwaway.java
@@ -3,24 +3,4 @@ import java.util.List;
 
 public class throwaway {
 
-    public static void main(String[] args) {
-        int inputSize = sc.nextInt();
-        List<Integer> list = new ArrayList<>();
-
-        int even = 1;
-        int odd = 1;
-        for (int i = 0; i < inputSize; i++) {
-            int current = sc.nextInt();
-            if(!(list.contains(current))) {
-                list.add(current);
-                if(current % 2 == 0) {
-                    even *= current;
-                } else {
-                    odd *= current;
-                }
-            }
-        }
-        int result = even - odd;
-        System.out.println(result);
-    }
 }
